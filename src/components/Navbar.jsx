568839a459aeb0681a5db15b579642777310b595
Kirthik24 <kirthikcode@gmail.com>
2023-04-25 09:57:24 +0530

    navbar responsiveness updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,32 +7,34 @@ const Navbar = () =>{
     return (
         <div className="navbar">
             <div className="image"></div>
-            <div className="navcontainer">
-                <div className={`nav-elements`}>
-                    <ul>
-                        <li>
-                            <NavLink to="/"> <div className='selectbox'>Profile</div> </NavLink> 
-                        </li>
-                        <li>
-                            <NavLink to="/myorders"> <div className='selectbox'>Teams</div> </NavLink> 
-                        </li>
-                        <li>
-                            <NavLink to="/research" > <div className='selectbox'>Research</div> </NavLink> 
-                        </li>
-                        <li>
-                            <NavLink to="/pb"> <div className='selectbox'>Publications</div> </NavLink> 
-                        </li>
-                        <li>
-                            <NavLink to="/login"> <div className='selectbox'>Facilities</div> </NavLink> 
-                        </li>
-                        <li>
-                            <NavLink to="/gallery"> <div className='selectbox'>Gallery</div> </NavLink> 
-                        </li>
-                        <li>
-                            <NavLink to="/login"> <div className='selectbox'>Contact</div> </NavLink> 
-                        </li>
-                    </ul>
-                </div>
+            <div className="navigationbar">
+                <div className="navcontainer">
+                    <div className={`nav-elements`}>
+                        <ul>
+                            <li>
+                                <NavLink to="/"> <div className='selectbox'>Profile</div> </NavLink> 
+                            </li>
+                            <li>
+                                <NavLink to="/myorders"> <div className='selectbox'>Teams</div> </NavLink> 
+                            </li>
+                            <li>
+                                <NavLink to="/research" > <div className='selectbox'>Research</div> </NavLink> 
+                            </li>
+                            <li>
+                                <NavLink to="/pb"> <div className='selectbox'>Publications</div> </NavLink> 
+                            </li>
+                            <li>
+                                <NavLink to="/login"> <div className='selectbox'>Facilities</div> </NavLink> 
+                            </li>
+                            <li>
+                                <NavLink to="/gallery"> <div className='selectbox'>Gallery</div> </NavLink> 
+                            </li>
+                            <li>
+                                <NavLink to="/login"> <div className='selectbox'>Contact</div> </NavLink> 
+                            </li>
+                        </ul>
+                    </div>
+            </div>
             </div>
         </div>
     )
