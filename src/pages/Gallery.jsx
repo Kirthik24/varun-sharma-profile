f30062915307af930b80f6b3ac1c30dfd90ffe87
Kirthik24 <kirthikcode@gmail.com>
2023-05-18 08:05:15 +0530

    gallery half done

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,6 +3,74 @@ import '../css/gallery.scss'
 import { g1,g2,g3,g4,g5,g6,g7,g8,g9,g10,g11,g12,g13,g14,g15,g16,g17,g18, g19 } from '../assets/gallery'
 
 const Gallery = () => {
+    return (
+        <div className="gcontainer">
+         <div className="gallery">
+          <div className='h-strech'><img src={g1} alt="" /></div>
+          <div className='v-strech'><img src={g2} alt="" /></div>
+          <div className='h-strech'><img src={g3} alt="" /></div>
+          <div className='v-strech'><img src={g4} alt="" /></div>
+          <div className='h-strech'><img src={g5} alt="" /></div>
+          <div className='h-strech'><img src={g6} alt="" /></div>
+          <div className='h-strech'><img src={g7} alt="" /></div>
+          <div className='h-strech'><img src={g8} alt="" /></div>
+          <div className='h-strech'><img src={g9} alt="" /></div>
+          <div className='h-strech'><img src={g10} alt="" /></div>
+          <div className='h-strech'><img src={g11} alt="" /></div>
+          <div className='h-strech'><img src={g12} alt="" /></div>
+          <div className='h-strech'><img src={g13} alt="" /></div>
+          <div className='h-strech'><img src={g14} alt="" /></div>
+          <div className='h-strech'><img src={g15} alt="" /></div>
+          <div className='h-strech'><img src={g16} alt="" /></div>
+          <div className='h-strech'><img src={g17} alt="" /></div>
+          <div className='h-strech'><img src={g18} alt="" /></div>
+          <div className='h-strech'><img src={g19} alt="" /></div>
+         
+         </div>
+        </div>
+       )
+     
+    
+}
+
+export default Gallery
+
+
+
+
+
+
+
+
+
+{/* <div className="gcontainer">
+<div className="gallery">
+    <h3>Gallery</h3>
+    <div className="row">
+        <div className="square"></div>
+        <div className="rectangle"></div>
+        <div className="square"></div>
+    </div>
+
+    <div className="row">
+        <div className="fullrectangle"></div>
+    </div>
+
+    <div className="row">
+        <div className="square"></div>
+        <div className="rectangle"></div>
+        <div className="square"></div>
+    </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
   return (
    <div className="gcontainer">
     <div className="images">
@@ -29,28 +97,5 @@ const Gallery = () => {
     </div>
    </div>
   )
-}
 
-export default Gallery
-
-
-{/* <div className="gcontainer">
-<div className="gallery">
-    <h3>Gallery</h3>
-    <div className="row">
-        <div className="square"></div>
-        <div className="rectangle"></div>
-        <div className="square"></div>
-    </div>
-
-    <div className="row">
-        <div className="fullrectangle"></div>
-    </div>
-
-    <div className="row">
-        <div className="square"></div>
-        <div className="rectangle"></div>
-        <div className="square"></div>
-    </div>
-</div>
-</div> */}
+*/}
